7bc4df75caa52fa76692a3d951ede01ce132fdd0
ToshiKajitsu <toshikajitsu@gmail.com>
2023-11-02 20:36:16 +0400

    select add

--- src/features/catCard/catCard.jsx
+++ src/features/catCard/catCard.jsx
@@ -5,17 +5,15 @@ import { useState } from 'react';
 export const CatCard = ({product}) => {
     const [selected, setSelected] = useState(false);
 
-    // const handleClick = () => {
-    //     if (product.isAvailable) {
-    //         setSelected((state) => !state)
-    //     };
-    // };
-    const setClass = () => {
+    const select = () => selected ? 'Selected' : 'Default';
+    const compound = () => selected ? 'Default' : 'Selected';
 
-    }
+    const handleClick = () => {
+        setSelected((currentValue) => !currentValue);
+    };
     return (
         <div className='catCard' >
-            <div className="catCardMain" onClick={setClass}>
+            <div className={`catCard_${select()}`}  onClick={handleClick}>
                 <p className='catCurdSubtitle'>Сказочное заморское явство</p>
                 <h2 className='catCardTitle'>Нямушка</h2>
                 <h3 className='catCardType'>{product.title}</h3>
@@ -26,18 +24,18 @@ export const CatCard = ({product}) => {
                     <p className="catCardWeight">{product.weight}</p>
                     <p className="catCardMeasure">кг</p>
                 </div>
-                {/* <p className='catCardComment'>
-                    {product.isAvailable (
-                        <span>Чего cидишь. порадуй котэ, <a href='./'>купи</a></span>
+            </div>    
+                <p className='catCardComment'>
+                    {product.isAvailable && (
+                        <span className={`prodectAvailable_${compound()}`} >Чего cидишь. порадуй котэ, <a href='./'>купи</a></span>
                     )}
-                    {product.isAvailable && <span>{product.comment}</span>}
+                    {product.isAvailable && selected && (<span className={`prodectAvailable_${select()}`} >{product.comment}</span>)}
                     {!product.isAvailable && (
                         <span>
                             Печалька, {product.title} закончился.
                         </span>
                     )}
-                </p> */}
-            </div>
+                </p>
         </div>
     )
 }
