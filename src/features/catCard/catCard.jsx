fa2a66e5df871fc84418fb6e1e9e87525adf722c
ToshiKajitsu <toshikajitsu@gmail.com>
2023-11-26 18:57:55 +0400

    fix: catCurdSubtitle

--- src/features/catCard/catCard.jsx
+++ src/features/catCard/catCard.jsx
@@ -4,17 +4,25 @@ import { useState } from 'react';
 
 export const CatCard = ({product}) => {
     const [selected, setSelected] = useState(false);
+    const [hovered, setHovered] = useState(true);
 
     const select = () => selected ? 'Selected' : 'Default';
     const compound = () => selected ? 'Default' : 'Selected';
-
+    const handleMouseEvent = () => {
+        if (selected) {
+            setHovered((currentValue) => !currentValue);
+        }
+    };
     const handleClick = () => {
         setSelected((currentValue) => !currentValue);
+        setHovered((currentValue) => currentValue = true);
+        
     };
     return (
         <div className='catCard' >
-            <div className={`catCard_${select()}`}  onClick={handleClick}>
-                <p className='catCurdSubtitle'>Сказочное заморское явство</p>
+            <div className={`catCard_${select()}`}  onClick={handleClick} onMouseLeave={handleMouseEvent}>
+                <p className={`catCurdSubtitle ${hovered ? 'activePgaragraph' : 'nonePgaragraph'}`}>Сказочное заморское явство</p>
+                <p className={`catCurdSubtitle redPgaragraph ${hovered ? 'nonePgaragraph' : 'activePgaragraph'}`} >Котэ на одобряет?</p>
                 <h2 className='catCardTitle'>Нямушка</h2>
                 <h3 className='catCardType'>{product.title}</h3>
                 <div className="catCardDescription">
@@ -27,11 +35,11 @@ export const CatCard = ({product}) => {
             </div>    
                 <p className='catCardComment'>
                     {product.isAvailable && (
-                        <span className={`prodectAvailable_${compound()}`} >Чего cидишь. порадуй котэ, <a href='./'>купи</a></span>
+                        <span className={`productAvailable_${compound()}`} >Чего cидишь. порадуй котэ, <a href='./'>купи</a></span>
                     )}
-                    {product.isAvailable && selected && (<span className={`prodectAvailable_${select()}`} >{product.comment}</span>)}
+                    {product.isAvailable && selected && (<span className={`productAvailable_${select()}`} >{product.comment}</span>)}
                     {!product.isAvailable && (
-                        <span>
+                        <span className='productNotAvailable'>
                             Печалька, {product.title} закончился.
                         </span>
                     )}
